40c3dc1659b422942f9c4493ea806449f616d408
EldrichMaid <kupo@mail.ru>
2024-08-08 14:05:50 +0300

    Добавлен цикл "do while"

--- LogicApp/Program.cs
+++ LogicApp/Program.cs
@@ -123,9 +123,9 @@ namespace LogicApp
                         break;
                 }
             }
+
             Console.WriteLine("Цикл while");
             int k = 0;
-
             while (k < 3)
             {
                 Console.WriteLine(k);
@@ -162,6 +162,44 @@ namespace LogicApp
                 k++;
 
             }
+            Console.WriteLine("Цикл do while");
+            int t = 0;
+            do {
+                Console.WriteLine(t);
+
+                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is red!");
+                        break;
+
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is green!");
+                        break;
+
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is cyan!");
+                        break;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is yellow!");
+                        break;
+                }
+                t++;
+            } 
+            while (t < 3);
 
             Console.ReadKey();
         }
