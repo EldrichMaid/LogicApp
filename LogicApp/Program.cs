206c2d381e09df51cb1bbf414f096efb71a1129e
EldrichMaid <kupo@mail.ru>
2024-08-08 11:28:28 +0300

    4.1.4 Добавлен код сравнения. В задании есть неточность:объявленным переменным требуется задать значение.

--- LogicApp/Program.cs
+++ LogicApp/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace LogicApp
+﻿using System.Net.Security;
+
+namespace LogicApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string A = "";
+            string B = "";
+            bool C = A != B;
+            Console.WriteLine(C);
+            Console.ReadKey();
         }
     }
 }
