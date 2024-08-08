59b0dde2ffa00ece2babb6ba8309c334cec26b29
EldrichMaid <kupo@mail.ru>
2024-08-08 14:00:44 +0300

    В цикл "for" возвращено условие. Добавлен цикл "while".

--- LogicApp/Program.cs
+++ LogicApp/Program.cs
@@ -90,7 +90,7 @@ namespace LogicApp
                     break;
             }
 
-            for (int i = 1;; i++)
+            for (int i = 1;i<5; i++)
             {
                 Console.WriteLine("Iteration {0}", i);
                 switch (Console.ReadLine())
@@ -123,6 +123,46 @@ namespace LogicApp
                         break;
                 }
             }
+            Console.WriteLine("Цикл while");
+            int k = 0;
+
+            while (k < 3)
+            {
+                Console.WriteLine(k);
+
+                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+                switch (Console.ReadLine())
+                {case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is yellow!");
+                    break; 
+                }
+                k++;
+
+            }
+
             Console.ReadKey();
         }
     }
