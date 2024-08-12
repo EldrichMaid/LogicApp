51882fa183711d59c47de6749d84dbe2fa60fe6f
EldrichMaid <kupo@mail.ru>
2024-08-12 14:00:54 +0300

    4.4.2 Создан кортеж

--- LogicApp/Program.cs
+++ LogicApp/Program.cs
@@ -422,6 +422,14 @@ namespace LogicApp
                 }
             }
 
+            (string name, int age) anketa;
+            Console.Write("Введите имя: ");
+            anketa.name = Console.ReadLine();
+            Console.Write("Введите возраст с цифрами: ");
+            anketa.age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ваше имя: {0}", anketa.name);
+            Console.WriteLine("Ваш возраст: {0}", anketa.age);
+
             Console.ReadKey();
         }
     }
