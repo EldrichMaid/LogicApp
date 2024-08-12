e652de702b1e71526b4a8e7effecf3661f07dc9b
EldrichMaid <kupo@mail.ru>
2024-08-12 11:56:23 +0300

    Добавлен массив,считающий количество букв и слове

--- LogicApp/Program.cs
+++ LogicApp/Program.cs
@@ -283,7 +283,20 @@ namespace LogicApp
                 }
             }
 
-                Console.ReadKey();
+            Console.WriteLine("Введите своё имя");
+
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Ваше имя по буквам: ");
+
+            foreach (var ch in name)
+            {
+                Console.Write(ch + " ");
+            }
+
+            Console.WriteLine("Последняя буква вашего имени: {0}", name[name.Length - 1]);
+
+            Console.ReadKey();
         }
     }
 }
