444c03fc5969a0adab85f0cbfa5b9e6f74f05e56
EldrichMaid <kupo@mail.ru>
2024-08-12 12:36:50 +0300

    Добавлен новый массив с отображением по стобцам

--- LogicApp/Program.cs
+++ LogicApp/Program.cs
@@ -325,6 +325,17 @@ namespace LogicApp
                 Console.WriteLine();
             }
 
+            Console.Write("А теперь пройдёмся по столбцам с новым массивом");
+            int[,] AnotherArray = { { 1, 2, 3 }, { 5, 6, 7 }, { 8, 9, 10 }, { 11, 12, 13 } };
+
+            for (int i = 0; i < AnotherArray.GetUpperBound(0) + 1; i++)
+            {
+                for (int n = 0; n < AnotherArray.GetUpperBound(1) + 1; n++)
+                    Console.Write(AnotherArray[n, i] + " ");
+
+                Console.WriteLine();
+            }
+
             Console.ReadKey();
         }
     }
